49cf3c993c3f49ec84452a4b9d51bdae98c44c7f
tony-molumby <25395668+tony-molumby@users.noreply.github.com>
2019-02-03 10:02:26 -0800

    2nd version ready for production

---
+++ precache-manifest.a73dd4e85bf5548bc7382bf4fbb05968.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "c932a1a5f46111933d18",
+    "url": "/static/css/main.368c3609.chunk.css"
+  },
+  {
+    "revision": "c932a1a5f46111933d18",
+    "url": "/static/js/main.c932a1a5.chunk.js"
+  },
+  {
+    "revision": "dac3e8a8966529c7a23d",
+    "url": "/static/js/1.dac3e8a8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "adbab7c17cf40e6e938cd5de4dca68b1",
+    "url": "/static/media/badaboom-bb.regular-webfont.adbab7c1.woff2"
+  },
+  {
+    "revision": "68ebf3ae14f6303b77c47c765f1d5266",
+    "url": "/static/media/badaboom-bb.regular-webfont.68ebf3ae.woff"
+  },
+  {
+    "revision": "46128326ec32ad6d10de044d34005642",
+    "url": "/static/media/comic_book_bold_italic-webfont.46128326.woff2"
+  },
+  {
+    "revision": "6cb69085462196d274e2b4d768a0344f",
+    "url": "/static/media/comic_book_bold-webfont.6cb69085.woff2"
+  },
+  {
+    "revision": "7941706c767716f54bb2a12bf779895c",
+    "url": "/static/media/comic_book_italic-webfont.7941706c.woff2"
+  },
+  {
+    "revision": "2bf45128af4b43b12ca888f633c1474b",
+    "url": "/static/media/comic_book-webfont.2bf45128.woff2"
+  },
+  {
+    "revision": "64f3194736b3d3fb069df384166997cd",
+    "url": "/static/media/comic_book_bold_italic-webfont.64f31947.woff"
+  },
+  {
+    "revision": "c33e2be6195eb75ef25363b4b142df15",
+    "url": "/static/media/comic_book_italic-webfont.c33e2be6.woff"
+  },
+  {
+    "revision": "a09696e2c99bbeb3246f8a1551fadb2a",
+    "url": "/static/media/comic_book_bold-webfont.a09696e2.woff"
+  },
+  {
+    "revision": "b074a3d8aedcff4458fbb503d38899b5",
+    "url": "/static/media/comic_book-webfont.b074a3d8.woff"
+  },
+  {
+    "revision": "491a33d0482061bab1372e3b3deff65e",
+    "url": "/static/media/contact.491a33d0.svg"
+  },
+  {
+    "revision": "bcd603008ee2858ebb1ef2b794415d17",
+    "url": "/static/media/headshot.bcd60300.svg"
+  },
+  {
+    "revision": "dc9c10fe14f78a462685762b6e43fd09",
+    "url": "/static/media/about.dc9c10fe.svg"
+  },
+  {
+    "revision": "3d4845447fdf44913ce7515b29a5085f",
+    "url": "/static/media/myWork.3d484544.svg"
+  },
+  {
+    "revision": "488786f62947a85034ef9de9bbdbae52",
+    "url": "/static/media/appStoreCard.488786f6.jpg"
+  },
+  {
+    "revision": "f17085ab185f5cf6c439c9f42d7b79ed",
+    "url": "/static/media/contactPage.f17085ab.png"
+  },
+  {
+    "revision": "75b73c2e0a82acf7635efa9dc67bca16",
+    "url": "/static/media/gameOfLife.75b73c2e.png"
+  },
+  {
+    "revision": "951ebc697c6211b90c9f6ceb18ad71bb",
+    "url": "/static/media/meetingSearch.951ebc69.png"
+  },
+  {
+    "revision": "0dc525b3cf91336b1c5b154a245cdcce",
+    "url": "/static/media/memoryGame.0dc525b3.png"
+  },
+  {
+    "revision": "8c29bce1a57bab37d4ca0b29e939cbf6",
+    "url": "/static/media/patientDashboard.8c29bce1.png"
+  },
+  {
+    "revision": "9e113bf4929022fe30384e6bddc9cc4d",
+    "url": "/static/media/patientList.9e113bf4.svg"
+  },
+  {
+    "revision": "42cab1cb97c7c39580219f6243930bd0",
+    "url": "/static/media/providerPortal.42cab1cb.png"
+  },
+  {
+    "revision": "b1ba983bba59cab6b0a638ffea8fc737",
+    "url": "/index.html"
+  }
+];
